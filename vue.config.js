a47082e4974254e8b035d9d2aec1fcdc3f44cc5d
priscila panes <priscila1777@gmail.com>
2024-05-15 12:27:58 -0400

    modifica

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/ejercicio-m6-s3-d8/",
-outputDir: "docs"
+  publicPath: "/perritoSombra/",
+  
+
 
 })
