cef525b49ec8015b414d2c49d57da21dcba95f07
priscila panes <priscila1777@gmail.com>
2024-05-15 12:42:52 -0400

    proyecto

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/perritoSombra/",
+  publicPath: "/ejercicio-m6-s3-d8/",
   
 
 
