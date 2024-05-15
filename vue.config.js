5f015db61a1d66fe439f3fa060985b2a6a0f11a1
priscila panes <priscila1777@gmail.com>
2024-05-15 12:05:53 -0400

    inicio proyecto

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: "/ejercicio-m6-s3-d8/",
+outputDir: "docs"
+
 })
